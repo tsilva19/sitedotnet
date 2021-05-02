79d2da567a24e85e22f84b8ba279359e300ff546
Thiago <thiagoalves060@gmail.com>
2021-05-02 16:24:16 -0300

    segundo commit

---
+++ bin/Debug/netcoreapp3.0/publish/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+// $('#formContato').on('submit', function(e){
+//   $('#modalSucesso').modal('show');
+//   e.preventDefault();
+// });
